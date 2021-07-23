9f9ca3ae7d52cd4480b7c02539df2a0d3d1b6d61
Elijah Cannon <Cclayelijah@gmail.com>
2021-07-23 13:37:05 -0600

    Delete Files directory

--- Files/setup.ps1
+++
@@ -1,6 +0,0 @@
-#Author: Elijah Cannon
-#West Coast Code Consultants
-#Sets up machine with file structures, cpuz helper files, and exported task
-
-Set-ExecutionPolicy -ExecutionPolicy Bypass
-Register-ScheduledTask -xml (Get-Content 'C:\WC3\BenchTest\WC3BenchmarkTask.xml' | Out-String) -TaskName "WC3 Benchmark" -TaskPath "\" -User SYSTEM –Force
