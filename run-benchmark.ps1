6ef662c996386941acda42988a424c3cc626cf3c
Elijah Cannon <Cclayelijah@gmail.com>
2021-07-27 14:21:18 -0600

    Add files via upload

--- run-benchmark.ps1
+++ run-benchmark.ps1
@@ -3,5 +3,11 @@
 #Runs the most up-to-date version from server
 
 Set-ExecutionPolicy -ExecutionPolicy Bypass
-$script = Invoke-WebRequest -Uri "https://plans.wc-3.com/path/to/script.ps1" -UseBasicParsing
-$script | Invoke-Expression
+try {
+    $script = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Cclayelijah/WC3Benchmark/main/Files/wc3-benchmark.ps1" -UseBasicParsing
+    $script | Invoke-Expression
+}
+catch {
+  Write-Host "An error occurred:"
+  Write-Host $_
+}
